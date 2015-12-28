14853c1b534b49d1ed0eedfb3ed134cbc0fc67fb
Konstantin Obenland <obenland@gmx.de>
2015-12-28 13:01:17 -0800

    Let's unlash it first.

--- shiny-updates.php
+++ shiny-updates.php
@@ -488,7 +488,7 @@ function wp_ajax_delete_plugin() {
 		wp_send_json_error( array( 'errorCode' => 'no_plugin_specified' ) );
 	}
 
-	$plugin      = filter_var( $_POST['plugin'], FILTER_SANITIZE_STRING );
+	$plugin      = filter_var( wp_unslash( $_POST['plugin'] ), FILTER_SANITIZE_STRING );
 	$plugin_data = get_plugin_data( WP_PLUGIN_DIR . '/' . $plugin );
 
 	$status = array(
