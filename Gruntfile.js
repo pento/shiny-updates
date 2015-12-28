da19b1c97ad36c38b613fe191a1d67a7ac261767
Konstantin Obenland <obenland@gmx.de>
2015-12-28 14:44:08 -0800

    Include Qunit in code style checks.
    Makes sure our unit tests adhere to coding guidelines as well. Except
    for vendor-specific files of course.

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,18 +26,22 @@ module.exports = function(grunt) {
 			tasks: ['jshint', 'qunit']
 		},
 		jscs: {
-			src: 'js/**/*.js',
-				options: {
-					verbose: true,
-					preset: 'wordpress'
-				}
+			src: [
+				'js/**/*.js',
+				'tests/**/*.js',
+				'!tests/vendor/**/*js'
+			],
+			options: {
+				verbose: true,
+				preset: 'wordpress'
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-qunit');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks( 'grunt-jscs' );
+	grunt.loadNpmTasks('grunt-jscs');
 
 	grunt.registerTask('default', ['jshint', 'jscs', 'qunit']);
 
