aa22ca4e80c72adb72f312ef665aa51572768e26
Pascal Birchler <pascal.birchler@gmail.com>
2016-06-06 19:52:22 +0200

    Make sure the re-install card cannot be empty (#258)
    Fixes #253.

--- src/functions.php
+++ src/functions.php
@@ -753,47 +753,53 @@ function su_update_table() {
 	if ( empty( $core_updates ) ) {
 		return;
 	}
-	?>
-	<div class="wordpress-reinstall-card card">
-		<h2><?php _e( 'Need to re-install WordPress?' ); ?></h2>
-		<?php
-		foreach ( $core_updates as $update ) :
-			if ( 'en_US' === $update->locale &&
-			     'en_US' === get_locale() ||
-			     (
-				     $update->packages->partial &&
-				     $wp_version === $update->partial_version &&
-				     1 === count( $core_updates )
-			     )
-			) {
-				$version_string = $update->current;
-			} else {
-				$version_string = sprintf( '%s&ndash;<code>%s</code>', $update->current, $update->locale );
-			}
 
-			if ( ! isset( $update->response ) || 'latest' === $update->response ) :
-			?>
-		<div class="wordpress-reinstall-card-item" data-type="core" data-reinstall="true" data-version="<?php echo esc_attr( $update->current ); ?>" data-locale="<?php echo esc_attr( $update->locale ); ?>">
-			<p>
-				<?php
-					/* translators: %s: WordPress version */
-					printf( __( 'If you need to re-install version %s, you can do so here.' ), $version_string );
-				?>
-			</p>
+	$first_pass = true;
+	foreach ( $core_updates as $update ) :
+		if ( 'en_US' === $update->locale &&
+		     'en_US' === get_locale() ||
+		     (
+			     $update->packages->partial &&
+			     $wp_version === $update->partial_version &&
+			     1 === count( $core_updates )
+		     )
+		) {
+			$version_string = $update->current;
+		} else {
+			$version_string = sprintf( '%s&ndash;<code>%s</code>', $update->current, $update->locale );
+		}
 
-			<form method="post" action="update-core.php?action=do-core-reinstall" name="upgrade" class="upgrade">
-				<?php wp_nonce_field( 'upgrade-core' ); ?>
-				<input name="version" value="<?php echo esc_attr( $update->current ); ?>" type="hidden"/>
-				<input name="locale" value="<?php echo esc_attr( $update->locale ); ?>" type="hidden"/>
-				<p>
-					<button type="submit" name="upgrade" class="button update-link"><?php esc_attr_e( 'Re-install Now' ); ?></button>
-				</p>
-			</form>
-		</div>
-		<?php endif; ?>
-		<?php endforeach; ?>
-	</div>
-	<?php
+		if ( ! isset( $update->response ) || 'latest' === $update->response ) :
+			if ( $first_pass ) : ?>
+				<div class="wordpress-reinstall-card card">
+					<h2><?php _e( 'Need to re-install WordPress?' ); ?></h2>
+			<?php endif; ?>
+
+					<div class="wordpress-reinstall-card-item" data-type="core" data-reinstall="true" data-version="<?php echo esc_attr( $update->current ); ?>" data-locale="<?php echo esc_attr( $update->locale ); ?>">
+						<p>
+							<?php
+								/* translators: %s: WordPress version */
+								printf( __( 'If you need to re-install version %s, you can do so here.' ), $version_string );
+							?>
+						</p>
+
+						<form method="post" action="update-core.php?action=do-core-reinstall" name="upgrade" class="upgrade">
+							<?php wp_nonce_field( 'upgrade-core' ); ?>
+							<input name="version" value="<?php echo esc_attr( $update->current ); ?>" type="hidden"/>
+							<input name="locale" value="<?php echo esc_attr( $update->locale ); ?>" type="hidden"/>
+							<p>
+								<button type="submit" name="upgrade" class="button update-link"><?php esc_attr_e( 'Re-install Now' ); ?></button>
+							</p>
+						</form>
+					</div>
+
+			<?php if ( $first_pass ) : ?>
+				</div>
+			<?php
+			$first_pass = false;
+			endif;
+		endif;
+	endforeach;
 }
 
 /**
