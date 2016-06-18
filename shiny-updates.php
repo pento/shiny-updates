224b6f26c5068863d2fbe570ec4840c5c54b154c
Pascal Birchler <hello@pascalbirchler.ch>
2016-06-18 12:46:59 +0200

    Add minimum WP version requirement

--- shiny-updates.php
+++ shiny-updates.php
@@ -5,14 +5,14 @@
  * Description: A smoother experience for managing plugins and themes.
  * Author: the WordPress team
  * Author URI: https://github.com/obenland/shiny-updates
- * Version: 2
+ * Version: 3
  * License: GPL2
  *
  * @package Shiny_Updates
  */
 
 /**
- * Init our plugin.
+ * Initializes the plugin.
  *
  * @codeCoverageIgnore
  */
@@ -23,4 +23,33 @@ function su_init() {
 	require_once( dirname( __FILE__ ) . '/src/default-filters.php' );
 }
 
-add_action( 'plugins_loaded', 'su_init' );
+function su_requirements_notice() {
+	$plugin_file = plugin_basename( __FILE__ );
+	?>
+	<div class="notice notice-warning is-dismissible">
+		<p>
+			<?php printf(
+				__( 'Please note: Shiny Updates v3 requires WordPress 4.6-alpha-37714 or higher. <a href="%s">Deactivate plugin</a>.' ),
+				wp_nonce_url(
+					add_query_arg(
+						array(
+							'action'        => 'deactivate',
+							'plugin'        => $plugin_file,
+							'plugin_status' => 'all',
+						),
+						admin_url( 'plugins.php' )
+					),
+					'deactivate-plugin_' . $plugin_file
+				)
+			); ?>
+		</p>
+	</div>
+	<?php
+}
+
+if ( version_compare( $GLOBALS['wp_version'], '4.6-alpha-37714', '<' ) ) {
+	add_action( 'admin_notices', 'su_requirements_notice' );
+} else {
+	add_action( 'plugins_loaded', 'su_init' );
+}
+
